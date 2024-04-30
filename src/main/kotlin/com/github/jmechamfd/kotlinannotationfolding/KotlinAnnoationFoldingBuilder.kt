8b60fa689229c8164415d7c82e205c5f7f7b1596
jmechamFD <jmecham@foundrydigital.com>
2024-04-29 21:03:46 -0400

    Add initial logic for annotation collapsing

--- src/main/kotlin/com/github/jmechamfd/kotlinannotationfolding/KotlinAnnoationFoldingBuilder.kt
+++ src/main/kotlin/com/github/jmechamfd/kotlinannotationfolding/KotlinAnnoationFoldingBuilder.kt
@@ -7,41 +7,42 @@ import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.idea.completion.argList
+import org.jetbrains.kotlin.psi.KtAnnotatedExpression
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
 import org.jetbrains.kotlin.psi.KtVisitorVoid
+import org.jetbrains.kotlin.psi.psiUtil.children
+import org.jetbrains.kotlin.psi.psiUtil.siblings
 import java.util.*
 
 class KotlinAnnotationFoldingBuilder : FoldingBuilderEx() {
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-        val descriptors = ArrayList<FoldingDescriptor>()
-        // Initialize and use a visitor to find and process annotation entries
-        if (root::class.simpleName == "KtFile") {
-            root as KtFile
-            root.accept(object : KtVisitorVoid() {
-                override fun visitAnnotationEntry(annotationEntry: KtAnnotationEntry) {
-                    super.visitAnnotationEntry(annotationEntry)
-                    val textRange =
-                        TextRange(annotationEntry.textRange.startOffset, annotationEntry.textRange.endOffset)
-                    // Check if the annotation spans multiple lines
-                    if (document.getLineNumber(textRange.startOffset) != document.getLineNumber(textRange.endOffset)) {
-                        val node = annotationEntry.node as com.intellij.lang.ASTNode
-                        descriptors.add(FoldingDescriptor(node, textRange))
-                    }
-                }
-            })
-        }
-        return descriptors.toArray(arrayOfNulls(descriptors.size))
+        val descriptors = mutableListOf<FoldingDescriptor>()
+        root.accept(object : KtTreeVisitorVoid() {
+            override fun visitAnnotationEntry(annotation: KtAnnotationEntry) {
+                super.visitAnnotationEntry(annotation)
+                checkAndAddFoldingDescriptor(annotation, document, descriptors)
+            }
+        })
+        return descriptors.toTypedArray()
     }
 
+    private fun checkAndAddFoldingDescriptor(annotationEntry: KtAnnotationEntry, document: Document, descriptors: MutableList<FoldingDescriptor>) {
+        val textRange = annotationEntry.textRange
+        if (document.getLineNumber(textRange.startOffset) != document.getLineNumber(textRange.endOffset)) {
+            descriptors.add(FoldingDescriptor(annotationEntry.node, textRange))
+        }
+    }
 
     override fun getPlaceholderText(node: ASTNode): String? {
         // Placeholder text displayed when the annotation is folded
-        return "@Annotation..."
+        val annotationName = node.psi.children[0].text
+        return String.format("@%s...", annotationName)
     }
 
     override fun isCollapsedByDefault(node: ASTNode): Boolean {
-        // You can customize this to make annotations folded by default or based on user preferences
-        return false
+        return true
     }
 }
